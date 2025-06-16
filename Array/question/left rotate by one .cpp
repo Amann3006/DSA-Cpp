3d028ade60f01ba1bb785dd92284c0091701fe7d
Amann3006 <amanvishwa024@gmail.com>
2025-06-16 22:36:59 +0530

    Left rotate by one

---
+++ Array/question/left rotate by one .cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+void leftrotate(int arr[],int n){
+    int temp=arr[0]; //store 1 element
+    for(int i=1; i<n; i++){
+        arr[i-1]=arr[i];
+        
+    }
+    //after loop {3,0,20,20}
+    arr[n-1]=temp;
+    //after this line
+    //{3,0,20,1}
+    
+}
+
+
+int main(){
+    int arr[]={1,3,0,20};
+    int n=4;
+
+    leftrotate(arr,n);
+
+    for(int i=0; i<n; i++ ){
+        cout<<arr[i]<<" ";
+    }
+}
