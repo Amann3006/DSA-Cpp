5d7362140d164d091497f66e098bcc825e0ed1a3
Amann3006 <amanvishwa024@gmail.com>
2026-01-16 12:41:23 +0530

    All bitwise Question

--- bitwiseopertion/check kth bit is set.cpp
+++ bitwiseopertion/check kth bit is set.cpp
@@ -1,62 +1,53 @@
 #include <iostream>
 using namespace std;
 
-//optimize
-
+// LEFT SHIFT
 void isset2(int n,int k){
-    int x=1<<(k-1);
-    
-    if((n&x)!=0){
-        cout<<"yes";
-
-    }
-    else{
-    cout<<"no";
-    }
+    int x = 1 << (k-1);
+    if(n & x) cout<<"yes";
+    else cout<<"no";
 }
 
-//efficient 
-void isset1(int n,int k){
-    
-    for(int i=0; i<(k-1); i++){
-        n=n/2;
-    }
-    if((n&1)!=0){
+// RIGHT SHIFT
+void isset3(int n,int k){
+    if(((n>>(k-1)) & 1) != 0)
         cout<<"yes";
+    else
+        cout<<"no";
+}
 
-    }
-    else{
-    cout<<"no";
-    }
+// DIVIDE METHOD
+void isset1(int n,int k){
+    for(int i=0;i<k-1;i++)
+        n = n/2;
+
+    if(n & 1)
+        cout<<"yes";
+    else
+        cout<<"no";
 }
 
 
 void isset(int n,int k){
-    int x=1;
-    for(int i=0; i<(k-1); i++){
-        x=x*2;
-    }
-    if((n&1)!=0){
-        cout<<"yes";
+    int x = 1;
+    for(int i=0;i<(k-1);i++)
+        x = x*2;
 
-    }
-    else{
-    cout<<"no";
-    }
+    if(n & x)
+        cout<<"yes";
+    else
+        cout<<"no";
 }
 
-
 int main(){
+    int n,k;
+    cin>>n>>k;
 
-   int n;
-   cin>>n;
-   int k;
-   cin>>k;
-
-   isset(n,k);
-   cout<<endl;
-  isset1(n,k);
-  cout<<endl;
-  isset2(n,k);
-
+    isset(n,k);
+    cout<<endl;
+    isset1(n,k);
+    cout<<endl;
+    isset2(n,k);
+    cout<<endl;
+    isset3(n,k);
 }
