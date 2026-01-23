3bc2425c95de6675199c9bf3ec229c2c446f2019
Amann3006 <amanvishwa024@gmail.com>
2026-01-23 18:04:55 +0530

    Bitswise question.cpp

--- bitwiseopertion/power set.cpp
+++ bitwiseopertion/power set.cpp
@@ -2,13 +2,13 @@
  #include<string.h>
  using namespace std;
    
-   
+   //tc(n*2^n)
    void printpower(string s){
     int n =s.length();
     int psize=(1<<n);
 
-    for(int i=0;  i<psize; i++){
-        for(int j=0; j<n; j++){
+    for(int i=0;  i<psize-1; i++){ ///->2^n
+        for(int j=0; j<n; j++){//->n
             if((i&(1<<j))!=0){
 
                 cout<<s[i]<<endl;;
@@ -25,6 +25,7 @@
  int main(){
 
     string s;
+    cin>>s;
        
     printpower(s);
  }
