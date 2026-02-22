8021bc8cfea7dd91d8772d52f32045f722275222
Amann3006 <amanvishwa024@gmail.com>
2026-02-22 18:06:56 +0530

    Stack

---
+++ Stack/PrefixToinfix.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+int main() {
+    string prefix;
+    cin >> prefix;
+
+    stack<string> st;
+
+    // Scan from right to left
+    for (int i = prefix.length() - 1; i >= 0; i--) {
+        char c = prefix[i];
+
+        // Operand
+        if (isalnum(c)) {
+            st.push(string(1, c));
+        }
+        // Operator
+        else if (isOperator(c)) {
+            string op1 = st.top(); st.pop();
+            string op2 = st.top(); st.pop();
+
+            string expr = "(" + op1 + c + op2 + ")";
+            st.push(expr);
+        }
+    }
+
+    cout << "Infix Expression: " << st.top();
+    return 0;
+}
+
