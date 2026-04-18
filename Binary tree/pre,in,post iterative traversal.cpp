7b69f2ec161a54e56dd8e5793dac9700969ee779
Amann3006 <amanvishwa024@gmail.com>
2026-04-18 13:17:52 +0530

    binary tree.cpp(traversal)

--- Binary tree/pre,in,post iterative traversal.cpp
+++ Binary tree/pre,in,post iterative traversal.cpp
@@ -32,6 +32,58 @@ vector<int> preordertraversal(node* root){
     return pre;
 }
 
+vector<int> inorderTraversal(node* root) {
+    vector<int> in;
+    stack<node*> st;
+    node* curr = root;
+
+    while (curr != NULL || !st.empty()) {
+        // Go to leftmost node
+        while (curr != NULL) {
+            st.push(curr);
+            curr = curr->left;
+        }
+
+        // Process node
+        curr = st.top();
+        st.pop();
+        in.push_back(curr->data);
+
+        // Move to right subtree
+        curr = curr->right;
+    }
+
+    return in;
+}
+
+vector<int> postorderTraversal(node* root) {
+    vector<int> post;
+    if (root == NULL) return post;
+
+    stack<node*> st;
+    node* curr = root;
+    node* lastVisited = NULL;
+
+    while (curr != NULL || !st.empty()) {
+        if (curr != NULL) {
+            st.push(curr);
+            curr = curr->left;
+        } else {
+            node* temp = st.top();
+
+            // if right child exists and not processed yet
+            if (temp->right != NULL && lastVisited != temp->right) {
+                curr = temp->right;
+            } else {
+                post.push_back(temp->data);
+                lastVisited = temp;
+                st.pop();
+            }
+        }
+    }
+
+    return post;
+}
 int main(){
     node* root = new node(1);
     root->left = new node(2);
@@ -40,6 +92,13 @@ int main(){
     root->left->right = new node(5);
 
     vector<int> ans = preordertraversal(root);
-
+    vector<int>ans1=inorderTraversal(root);
+    vector<int>ans2=postorderTraversal(root);
+    cout<<"preorder"<<endl;
     for(auto i : ans) cout << i << " ";
+    cout<<endl<<"inorder"<<endl;
+    for(auto i : ans1) cout << i << " ";
+    cout<<endl<<"postorder"<<endl;
+    for(auto i : ans2) cout << i << " ";
+    
 }
